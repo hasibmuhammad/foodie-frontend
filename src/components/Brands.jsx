fbe5286b789918a6ebaba6148d361bdf1d3325c0
hasibmuhammad <hasibmu99@gmail.com>
2023-12-03 20:27:02 +0600

    Brands Loaded from database

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Brands = () => {
+  const [brands, setBrands] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/brands")
+      .then((res) => res.json())
+      .then((data) => {
+        setBrands(data);
+      });
+  }, []);
+
+  console.log(brands);
   return (
-    <div className="max-w-7xl mx-auto my-10">
+    <div className="max-w-7xl mx-auto my-10 px-10 lg:px-0">
       <h1 className="text-center font-extrabold text-5xl">Our Brands</h1>
       <div className="flex justify-center items-center">
         <hr className="mt-4 border border-b-4 border-primary w-32" />
+      </div>
+      <div className="my-20 grid grid-cols-1 md:grid-cols-6 place-items-center gap-4">
+        {brands.map((brand) => (
+          <Link>
+            <div className="text-center" key={brand._id}>
+              <img
+                className="w-24 h-24 object-cover"
+                src={brand.brandPhoto}
+                alt={brand.brandName}
+              />
+              <p className="font-semibold text-gray-500">{brand.brandName}</p>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
